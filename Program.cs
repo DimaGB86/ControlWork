afef5061d76b76fbf8e897c86e476ca312cd625b
Dima <sh_diman@inbox.ru>
2023-04-29 15:56:41 +0500

    Добавил еще ветку и второе решение задачи

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
 // выполнения алгоритма. При решении не рекомендуется пользоваться 
 // коллекциями, лучше обойтись исключительно массивами.
 
-// Решение:
-
+// Решение пользователь сам вводит.
+/*
 string[] CreateStringArray()
 {
     Console.WriteLine("Введите количество элементов в массиве: ");
@@ -51,3 +51,32 @@ string[] myArray2 = LessThenThreeSymbols(myArray);
 ShowArray(myArray);
 Console.WriteLine("");
 ShowArray(myArray2);
+*/
+
+// Решение №2. заданный массив.
+/*
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
+*/
